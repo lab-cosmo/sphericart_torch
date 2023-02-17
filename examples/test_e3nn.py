e4bdf5ea1272b328fb239611ae88f3a9ee1907ff
nickjbrowning <nickjbrowning@gmail.com>
2023-02-17 11:06:15 +0100

    fixed an issue with test_e3nn

--- examples/test_e3nn.py
+++ examples/test_e3nn.py
@@ -5,24 +5,29 @@ from scipy import special
 import torch_sh
 import e3nn
 
-x = torch.rand(10)
-y = torch.rand(10)
-z = torch.rand(10)
+torch.set_default_dtype(torch.float64)
+xyz = torch.rand(10, 3)
 
-def test_sh_against_e3nn(x: np.ndarray, y: np.ndarray, z: np.ndarray, l_max: int):
+def test_sh_against_e3nn(xyz: torch.Tensor, l_max: int):
 
     # e3nn spherical harmonics:
-    all_together = torch.stack([y, z, x], dim=1)
-    sh_e3nn = e3nn.o3.spherical_harmonics(range(l_max+1), all_together, normalize=True, normalization='integral')
+    #all_together = torch.stack([y, z, x], dim=1)
+    
+    sh_e3nn = e3nn.o3.spherical_harmonics(range(l_max+1), xyz, normalize=True, normalization='integral')
 
+    print (sh_e3nn, sh_e3nn.shape)
+    
     # sphericart spherical harmonics:
     sh_calculator = torch_sh.SphericalHarmonics(l_max, "cpu")
-    sh_sphericart = sh_calculator.compute(l_max, x, y, z)
+    sh_sphericart = sh_calculator.compute(xyz)
 
+    print (sh_sphericart, sh_sphericart.shape)
+    
     for l in range(l_max+1):
+        print (sh_sphericart[l])
         assert torch.allclose(sh_sphericart[l], sh_e3nn[:, l**2:(l+1)**2])
 
-l_max = 6
-test_sh_against_e3nn(x, y, z, l_max)
+l_max = 3
+test_sh_against_e3nn(xyz, l_max)
 
 print("Assertions passed successfully!")
--- torch_sh/spherical_harmonics.py
+++ torch_sh/spherical_harmonics.py
@@ -10,6 +10,8 @@ class SphericalHarmonics:
         self.prefactors = compute_prefactors(l_max, device)
 
     def compute(self, xyz):
+        
+        #print (self.prefactors.dtype)
         Y_tilde = spherical_harmonics(self.l_max, self.prefactors, xyz)
 
         r = torch.sqrt(torch.sum(xyz**2, dim = 1))
