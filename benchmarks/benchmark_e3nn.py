db97b6fcd461e2a3aa0abe77067571d6b120691e
Michele Ceriotti <michele.ceriotti@gmail.com>
2023-02-17 17:26:55 +0100

    Renaming of the library and use requires_grad() to decide whether to compute derivatives in the fw pass

--- benchmarks/benchmark_e3nn.py
+++ benchmarks/benchmark_e3nn.py
@@ -10,7 +10,7 @@ l_max = 10
 #yzx = torch.rand((100000, 3), device=device, requires_grad=False)
 #sh = e3nn.o3.spherical_harmonics(range(l_max+1), yzx, normalize=True, normalization='integral')
 
-yzx = torch.rand((10000, 3), device=device, requires_grad=True)
+yzx = torch.rand((1000000, 3), device=device, requires_grad=True)
 
 print("Forward pass")
 start_time = time.time()
--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 from torch.utils import cpp_extension
 
 spherical_harmonics_extension = cpp_extension.CppExtension("spherical_harmonics_extension", 
-      ["torch_sh/extension/spherical_harmonics.cc"], 
-      extra_compile_args=["-std=c++17", "-fopenmp", "-Wall"])
+      ["sphericart_torch/extension/spherical_harmonics.cc"], 
+      extra_compile_args=["-std=c++17", "-O3", "-fopenmp", "-Wall"])
 
 ext_modules = [spherical_harmonics_extension]
 
-setup(name="torch_sh",
+setup(name="sphericart_torch",
       packages = find_packages(),
       ext_modules = ext_modules,
       cmdclass={"build_ext": cpp_extension.BuildExtension})
