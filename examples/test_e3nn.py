5481e039b0893e496e43a11601fefc4c747682b0
frostedoyster <bigi.f@libero.it>
2023-02-17 20:04:23 +0100

    Final e3nn test fix

--- examples/test_e3nn.py
+++ examples/test_e3nn.py
@@ -11,23 +11,17 @@ xyz = torch.rand(10, 3)
 def test_sh_against_e3nn(xyz: torch.Tensor, l_max: int):
 
     # e3nn spherical harmonics:
-    #all_together = torch.stack([y, z, x], dim=1)
+    e3nn_xyz = xyz[:, [1, 2, 0]]   # e3nn y, z, x ordering
     
-    sh_e3nn = e3nn.o3.spherical_harmonics(range(l_max+1), xyz, normalize=True, normalization='integral')
-
-    print (sh_e3nn, sh_e3nn.shape)
+    sh_e3nn = e3nn.o3.spherical_harmonics(range(l_max+1), e3nn_xyz, normalize=True, normalization='integral')
     
     # sphericart spherical harmonics:
     sh_calculator = torch_sh.SphericalHarmonics(l_max, "cpu")
     sh_sphericart = sh_calculator.compute(xyz)
-
-    print (sh_sphericart, sh_sphericart.shape)
     
-    for l in range(l_max+1):
-        print (sh_sphericart[l])
-        assert torch.allclose(sh_sphericart[l], sh_e3nn[:, l**2:(l+1)**2])
+    assert torch.allclose(sh_sphericart, sh_e3nn)
 
-l_max = 3
+l_max = 6
 test_sh_against_e3nn(xyz, l_max)
 
 print("Assertions passed successfully!")
